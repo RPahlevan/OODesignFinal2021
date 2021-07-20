e0f84e1b58dcbf8f0c1e8021426bd1579e1275b8
esiumat <matthew.siu@ericsson.com>
2021-07-20 17:15:52 -0400

    Add concrete classes for NokiaLTE radios & factories [WIP]

--- src/main/java/radiounit/NokiaLteRadioUnitReceiver.java
+++ src/main/java/radiounit/NokiaLteRadioUnitReceiver.java
@@ -1,6 +1,7 @@
 package radiounit;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import common.Carrier;
 import common.FrequencyBand;
@@ -13,56 +14,62 @@ import common.FrequencyBand;
  */
 public class NokiaLteRadioUnitReceiver implements RadioUnitReceiver {
 
-	private List<Carrier> carriers;
+	private static final int CARRIER_NOT_FOUND_IDX = -1;
+
+	/*
+	 * The list of carriers is an important part of the radio
+	 * 
+	 * The "volatile" keyword ensures that this resource is not cached, and is
+	 * retrieved directly from memory every time it is accessed.
+	 * 
+	 * The three "-Internal" methods below are all synchronized and have exclusive
+	 * access to this resource.
+	 */
+	private volatile List<Carrier> carriers;
+
+	public NokiaLteRadioUnitReceiver() {
+		this.carriers = new ArrayList<Carrier>();
+	}
 
 	public void setupNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] setupNokiaLte");
 	}
 
 	public void activateNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] activateNokiaLte");
 	}
 
 	public void deactivateNokiaLte() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("[NokiaLteRadioUnitReceiver] deactivateNokiaLte");
 	}
 
 	public void releaseNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] releaseNokiaLte");
 	}
 
 	public void setupCarrierNokiaLte(Carrier carrier) {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] setupCarrierNokiaLte: " + carrier);
 	}
 
 	public void signalScalingNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] signalScalingNokiaLte");
 	}
 
 	public void modifyCarrierNokiaLte(Integer carrierId, FrequencyBand frequencyBand) {
-		// TODO Auto-generated method stub
-
+		System.out.println(
+				"[NokiaLteRadioUnitReceiver] modifyCarrierNokiaLte: " + carrierId + ", " + frequencyBand.getBand());
 	}
 
 	public void removeCarrierNokiaLte(Integer carrierId) {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] removeCarrierNokiaLte: " + carrierId);
 	}
 
 	public void selfDiagnosticsNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] selfDiagnosticsNokiaLte");
 	}
 
 	public void removeAllCarriersNokiaLte() {
-		// TODO Auto-generated method stub
-
+		System.out.println("[NokiaLteRadioUnitReceiver] removeAllCarriersNokiaLte");
 	}
 
 	public List<Carrier> getCarriers() {
