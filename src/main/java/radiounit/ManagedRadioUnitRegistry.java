5651c2825a5372d6ecf7ef156e2830987fe9f645
esiumat <matthew.siu@ericsson.com>
2021-07-23 12:05:35 -0400

    Add addRadioUnit functionality

--- src/main/java/radiounit/ManagedRadioUnitRegistry.java
+++ src/main/java/radiounit/ManagedRadioUnitRegistry.java
@@ -23,66 +23,63 @@ public class ManagedRadioUnitRegistry extends AbstractManagedRadioUnitRegistry {
 
 	@Override
 	public void addRadioUnit(String ipAddress, String name, Vendor vendor, RatType ratType) {
-		// TODO need to add RadioUnit here
+		// check that radio hasn't already been added
+		if (getByIpAddress(ipAddress).isEmpty()) {
+			System.err.println("A radio with this IP address has already been added");
+			return;
+		}
 
+		radioUnits.add(new ConcreteRadioUnit(ipAddress, name, vendor, ratType));
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByIpAddress(String ipAddress) {
 		List<ManagedRadioUnit> radioUnitsFilteredByIpAddress = radioUnits.stream()
-				.filter(ru -> ru.getIpAddress().equals(ipAddress))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getIpAddress().equals(ipAddress)).collect(Collectors.toList());
 		return radioUnitsFilteredByIpAddress;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByName(String name) {
 		List<ManagedRadioUnit> radioUnitsFilteredByName = radioUnits.stream()
-				.filter(ru -> ru.getRadioUnitName().equals(name))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getRadioUnitName().equals(name)).collect(Collectors.toList());
 		return radioUnitsFilteredByName;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByVendor(Vendor vendor) {
 		List<ManagedRadioUnit> radioUnitsFilteredByVendor = radioUnits.stream()
-				.filter(ru -> ru.getVendor().equals(vendor))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getVendor().equals(vendor)).collect(Collectors.toList());
 		return radioUnitsFilteredByVendor;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByRatType(RatType ratType) {
 		List<ManagedRadioUnit> radioUnitsFilteredByRatType = radioUnits.stream()
-				.filter(ru -> ru.getRatType().equals(ratType))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getRatType().equals(ratType)).collect(Collectors.toList());
 		return radioUnitsFilteredByRatType;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByAlarmStatus(AlarmStatusLevel alarmStatus) {
 		List<ManagedRadioUnit> radioUnitsFilteredByAlarmStatus = radioUnits.stream()
-				.filter(ru -> ru.getVendor().equals(alarmStatus))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getVendor().equals(alarmStatus)).collect(Collectors.toList());
 		return radioUnitsFilteredByAlarmStatus;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getByState(RadioUnitState state) {
 		List<ManagedRadioUnit> radioUnitsFilteredByState = radioUnits.stream()
-				.filter(ru -> ru.getCurrentState().equals(state))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getCurrentState().equals(state)).collect(Collectors.toList());
 		return radioUnitsFilteredByState;
 	}
 
 	@Override
 	public List<ManagedRadioUnit> getSpecificRadio(String ipAddress, String name, Vendor vendor, RatType ratType) {
 		List<ManagedRadioUnit> radioUnitsFiltered = radioUnits.stream()
-				.filter(ru -> ru.getIpAddress().equals(ipAddress))
-				.filter(ru -> ru.getRadioUnitName().equals(name))
-				.filter(ru -> ru.getVendor().equals(vendor))
-				.filter(ru -> ru.getRatType().equals(ratType))
-				.collect(Collectors.toList());		
+				.filter(ru -> ru.getIpAddress().equals(ipAddress)).filter(ru -> ru.getRadioUnitName().equals(name))
+				.filter(ru -> ru.getVendor().equals(vendor)).filter(ru -> ru.getRatType().equals(ratType))
+				.collect(Collectors.toList());
 		return radioUnitsFiltered;
 	}
 
